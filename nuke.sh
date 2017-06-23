c033d72dad8af9053d6764717adcc6836c919962
Matheus Andrade <matheus.andrade@webedia-group.com>
2017-06-22 21:35:49 -0300

    adding confirmation prompt

--- nuke.sh
+++ nuke.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
-rm -rf ~/.config/
-rm -rf ~/.cache/
-rm -rf ~/.ssh/
+read -p "Are you sure? " -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+  rm -rf ~/.config/
+  rm -rf ~/.cache/
+  rm -rf ~/.ssh/
+fi
+
